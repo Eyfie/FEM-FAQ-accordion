56a36a8a1796a510fc6f7b72415407aaf37016c2
Thomas Mendez de Vigo <Eyfie@mbp-de-thomas.home>
2024-01-09 19:12:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {css} from '../styled-system/css'
+
+
+function App() {
+
+  return (
+    <>
+     <h1 className={css({color: 'blue.500'})}>Hello world</h1>
+    </>
+  )
+}
+
+export default App
